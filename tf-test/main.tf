b643b5cec18d4f7acb263119918d7895110ce94a
Michael DeNino <93542256+michael-denino@users.noreply.github.com>
2022-11-19 18:52:22 -0500

    update tf-test placeholder name

--- tf-test/main.tf
+++ tf-test/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 1.0.0"
   backend "s3" {
-    bucket         = "<bucket-name>"
+    bucket         = "<s3-bucket-name>"
     key            = "<state-file-name>.tfstate"
     dynamodb_table = "<dynamodb-table-name>"
     region         = "<aws-region>"
