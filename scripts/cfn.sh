32a3938028ed66df6666edaecfdac31ed2e3c551
Michael DeNino <93542256+michael-denino@users.noreply.github.com>
2022-11-19 21:16:23 -0500

    update cfn script

--- scripts/cfn.sh
+++ scripts/cfn.sh
@@ -3,19 +3,30 @@
 set -o nounset
 set -o errexit
 
-# pass the cloudformation command as the first arguemnt (e.g create-stack, update-stack, etc.)
+# pass the cloudformation command as the first arguemnt (e.g create-stack, update-stack, delete-stack, etc.)
 # pass the environment name as the second arguemnt (e.g dev, prod, etc.)
-# pass the project name as the third arguemnt (must be unique as S3 buckets are globally unique)
-# i.e. ./stack.sh create-stack prod company-name
+# pass a purpose description as the third arguemnt
+# i.e. $ ./stack.sh create-stack dev test
 COMMAND=$1
 ENVIRONMENT=$2
-PROJECT=$3
+PURPOSE=$3
+STACK_NAME=$ENVIRONMENT-$PURPOSE-terraform-backend
+AWS_ACCOUNT=$(aws sts get-caller-identity --query Account --output text)
 
-# Stack-level tags are applied to all supported resources in the CloudFormation stack
-aws cloudformation $COMMAND \
---stack-name $ENVIRONMENT-$PROJECT-terraform-backend \
---template-body file://../tf-s3-backend.yaml \
---parameters ParameterKey=StateBucketName,ParameterValue=$ENVIRONMENT-$PROJECT-terraform-backend \
-             ParameterKey=LockTableName,ParameterValue=$ENVIRONMENT-$PROJECT-terraform-backend \
---tags Key=Name,Value=$ENVIRONMENT-$PROJECT-terraform-backend \
-       Key=environment,Value=$ENVIRONMENT
+case $COMMAND in
+    "delete-stack" | "describe-stacks")
+        aws cloudformation $COMMAND \
+            --stack-name $STACK_NAME
+        ;;
+    *)
+        # Stack-level tags are applied to all supported resources in the CloudFormation stack
+        aws cloudformation $COMMAND \
+            --stack-name $STACK_NAME \
+            --template-body file://../tf-s3-backend.yaml \
+            --parameters ParameterKey=StateBucketName,ParameterValue=$STACK_NAME-$AWS_ACCOUNT \
+            ParameterKey=LockTableName,ParameterValue=$STACK_NAME \
+            --tags Key=Name,Value=$STACK_NAME \
+            Key=environment,Value=$ENVIRONMENT \
+            Key=purpose,Value=$PURPOSE
+        ;;
+esac
